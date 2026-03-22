1a6b4666da688ddf06319270df987670ac468a0c
Ianzb <93322252@qq.com>
2026-03-22 20:25:53 +0800

    feat: 缩减发包体积

--- scripts/release.py
+++ scripts/release.py
@@ -3,3 +3,6 @@ from rule import *
 save_file("module/immerse_rules_33.json", importFromOS33("module/immerse_rules.json").toData("33"))
 save_file("module/immerse_rules_30.json", importFromOS33("module/immerse_rules.json").toData("30"))
 save_file("module/immerse_rules_22.xml", importFromOS33("module/immerse_rules.json").toData("22"))
+
+import os
+os.remove("module/immerse_rules.json")
