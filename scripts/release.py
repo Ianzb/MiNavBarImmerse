03df7ca555a13a98cab01f199900671d807053f4
Ianzb <93322252@qq.com>
2026-03-30 06:37:05 +0800

    fix: 意外删除配置文件的Bug

--- scripts/release.py
+++ scripts/release.py
@@ -1,8 +1,7 @@
 from rule import *
 
+
+save_file("module/immerse_rules.json", importFromOS33("module/immerse_rules.json").toData("33"))
 save_file("module/immerse_rules_33.json", importFromOS33("module/immerse_rules.json").toData("33"))
 save_file("module/immerse_rules_30.json", importFromOS33("module/immerse_rules.json").toData("30"))
 save_file("module/immerse_rules_22.xml", importFromOS33("module/immerse_rules.json").toData("22"))
-
-import os
-os.remove("module/immerse_rules.json")
--- scripts/sort_rule.py
+++
@@ -1,3 +0,0 @@
-from rule import *
-
-save_file("module/immerse_rules.json", importFromOS33("module/immerse_rules.json").toData("33"))
